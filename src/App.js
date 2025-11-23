e14d39a16ad55cc5ec88efcba5e742a6d61db83e
Kaiwen Yao <kaiwen.yao.cn@outlook.com>
2025-11-23 17:48:50 +0000

    完成第一个任务-评论相关

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 // 项目的根组件
+const list = [
+  { id: 1001, name: "vue" },
+  { id: 1002, name: "react" },
+  { id: 1003, name: "Angular" },
+];
 function App() {
-  return <div className="App">This is app.</div>;
+  return (
+    <div className="App">
+      This is app.
+      <ul>
+        {list.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
