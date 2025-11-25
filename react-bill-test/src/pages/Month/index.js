e52b27ab32c70f586785d5e2cc41209f8062c330
Kaiwen Yao <kaiwen.yao.cn@outlook.com>
2025-11-25 21:18:57 +0000

    账单页面弹窗和切换箭头

--- react-bill-test/src/pages/Month/index.js
+++ react-bill-test/src/pages/Month/index.js
@@ -1,7 +1,14 @@
-import { NavBar, DatePicker } from 'antd-mobile'
-import './index.scss'
-
+import { NavBar, DatePicker } from "antd-mobile";
+import "./index.scss";
+import { useState } from "react";
+import classNames from "classnames";
 const Month = () => {
+  // 控制弹窗的打开和关闭
+  const [dateVisible, setDateVisible] = useState(false);
+  const onConfirm = () => {
+    setDateVisible(false);
+    // 其他逻辑
+  };
   return (
     <div className="monthlyBill">
       <NavBar className="nav" backArrow={false}>
@@ -10,14 +17,15 @@ const Month = () => {
       <div className="content">
         <div className="header">
           {/* 时间切换区域 */}
-          <div className="date">
-            <span className="text">
-              2023 | 3月账单
-            </span>
-            <span className='arrow expand'></span>
+          <div className="date" onClick={() => setDateVisible(true)}>
+            <span className="text">2023 | 3月账单</span>
+            {/* 根据类名 控制箭头上下 expand */}
+            <span
+              className={classNames("arrow", dateVisible && "expand")}
+            ></span>
           </div>
           {/* 统计区域 */}
-          <div className='twoLineOverview'>
+          <div className="twoLineOverview">
             <div className="item">
               <span className="money">{100}</span>
               <span className="type">支出</span>
@@ -36,13 +44,16 @@ const Month = () => {
             className="kaDate"
             title="记账日期"
             precision="month"
-            visible={false}
+            visible={dateVisible}
+            onCancel={() => setDateVisible(false)}
+            onConfirm={onConfirm}
+            onClose={() => setDateVisible(false)}
             max={new Date()}
           />
         </div>
       </div>
-    </div >
-  )
-}
+    </div>
+  );
+};
 
-export default Month
+export default Month;
