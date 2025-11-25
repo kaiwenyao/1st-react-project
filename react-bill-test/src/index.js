7f883e44a5d625c78c3e592f6c1d2f7cb355d845
Kaiwen Yao <kaiwen.yao.cn@outlook.com>
2025-11-25 20:35:36 +0000

    添加billstore使用RTK

--- react-bill-test/src/index.js
+++ react-bill-test/src/index.js
@@ -4,5 +4,13 @@ import "./index.css";
 import App from "./App";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import "./theme.css";
+import { Provider } from "react-redux";
+import store from "./store";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
+root.render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+);
--- react-bill-test/src/pages/Layout/index.js
+++ react-bill-test/src/pages/Layout/index.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
-
+import { Button } from "antd-mobile";
+import { useDispatch } from "react-redux";
+import { getBillList } from "@/store/modules/billStore";
 const Layout = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBillList());
+  }, [dispatch]);
   return (
     <div>
       <Outlet />
       Layout
+      {/* 测试全局配置 */}
+      <Button color="primary">test global</Button>
+      <div className="purple">
+        <Button color="primary">test local</Button>
+      </div>
     </div>
   );
 };
