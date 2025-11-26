2d161fd402255a8f78e0145477b54c01364cb27f
Kaiwen Yao <kaiwen.yao.cn@outlook.com>
2025-11-26 16:36:46 +0000

    修改按钮样式

--- react-bill-test/src/pages/New/index.js
+++ react-bill-test/src/pages/New/index.js
@@ -1,12 +1,15 @@
-import { Button, DatePicker, Input, NavBar } from 'antd-mobile'
-import Icon from '@/components/Icon'
-import './index.scss'
-import classNames from 'classnames'
-import { billListData } from '@/constants'
-import { useNavigate } from 'react-router-dom'
+import { Button, DatePicker, Input, NavBar } from "antd-mobile";
+import Icon from "@/components/Icon";
+import "./index.scss";
+import classNames from "classnames";
+import { billListData } from "@/constants";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const New = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  // 控制收入/支出切换的状态
+  const [billType, setBillType] = useState("pay"); // pay 支出 income 收入
   return (
     <div className="keepAccounts">
       <NavBar className="nav" onBack={() => navigate(-1)}>
@@ -16,14 +19,16 @@ const New = () => {
       <div className="header">
         <div className="kaType">
           <Button
+            onClick={() => setBillType("pay")}
             shape="rounded"
-            className={classNames('selected')}
+            className={classNames(billType === "pay" && "selected")}
           >
             支出
           </Button>
           <Button
-            className={classNames('')}
+            onClick={() => setBillType("income")}
             shape="rounded"
+            className={classNames(billType === "income" && "selected")}
           >
             收入
           </Button>
@@ -33,7 +38,7 @@ const New = () => {
           <div className="kaForm">
             <div className="date">
               <Icon type="calendar" className="icon" />
-              <span className="text">{'今天'}</span>
+              <span className="text">{"今天"}</span>
               <DatePicker
                 className="kaDate"
                 title="记账日期"
@@ -41,11 +46,7 @@ const New = () => {
               />
             </div>
             <div className="kaInput">
-              <Input
-                className="input"
-                placeholder="0.00"
-                type="number"
-              />
+              <Input className="input" placeholder="0.00" type="number" />
               <span className="iconYuan">¥</span>
             </div>
           </div>
@@ -53,41 +54,33 @@ const New = () => {
       </div>
 
       <div className="kaTypeList">
-        {billListData['pay'].map(item => {
+        {/* 数据区域 */}
+        {billListData[billType].map((item) => {
           return (
             <div className="kaType" key={item.type}>
               <div className="title">{item.name}</div>
               <div className="list">
-                {item.list.map(item => {
+                {item.list.map((item) => {
                   return (
-                    <div
-                      className={classNames(
-                        'item',
-                        ''
-                      )}
-                      key={item.type}
-
-                    >
+                    <div className={classNames("item", "")} key={item.type}>
                       <div className="icon">
                         <Icon type={item.type} />
                       </div>
                       <div className="text">{item.name}</div>
                     </div>
-                  )
+                  );
                 })}
               </div>
             </div>
-          )
+          );
         })}
       </div>
 
       <div className="btns">
-        <Button className="btn save">
-          保 存
-        </Button>
+        <Button className="btn save">保 存</Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default New
+export default New;
