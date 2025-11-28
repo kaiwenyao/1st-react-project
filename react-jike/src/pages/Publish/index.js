0fd12dae3e5364658a856b3bf8f6214bcecc2b35
Kaiwen Yao <kaiwen.yao.cn@outlook.com>
2025-11-28 14:09:59 +0000

    限制图片最大上传数量

--- react-jike/src/pages/Publish/index.js
+++ react-jike/src/pages/Publish/index.js
@@ -47,10 +47,14 @@ const Publish = () => {
   };
   const [imageList, setImageList] = useState([]);
   // 上传回调
-
   const onChange = (value) => {
     // console.log("uploadddddd");
     setImageList(value.fileList);
+  };
+  // 切换图片类型
+  const [imageType, setImageType] = useState(0);
+  const onTypeChange = (e) => {
+    setImageType(e.target.value);
   };
   return (
     <div className="publish">
@@ -67,7 +71,7 @@ const Publish = () => {
         <Form
           labelCol={{ span: 4 }}
           wrapperCol={{ span: 16 }}
-          initialValues={{ type: 1 }}
+          initialValues={{ type: imageType}}
           onFinish={onFinish}
         >
           <Form.Item
@@ -92,7 +96,7 @@ const Publish = () => {
           </Form.Item>
           <Form.Item label="封面">
             <Form.Item name="type">
-              <Radio.Group>
+              <Radio.Group onChange={onTypeChange}>
                 <Radio value={1}>单图</Radio>
                 <Radio value={3}>三图</Radio>
                 <Radio value={0}>无图</Radio>
@@ -101,17 +105,20 @@ const Publish = () => {
             {/* listType决定样式
             showUploadList 控制显示上传列表
              */}
-            <Upload
-              listType="picture-card"
-              showUploadList
-              action={"http://geek.itheima.net/v1_0/upload"}
-              name="image"
-              onChange={onChange}
-            >
-              <div style={{ marginTop: 8 }}>
-                <PlusOutlined />
-              </div>
-            </Upload>
+            {imageType > 0 && (
+              <Upload
+                listType="picture-card"
+                showUploadList
+                action={"http://geek.itheima.net/v1_0/upload"}
+                name="image"
+                onChange={onChange}
+                maxCount={imageType}
+              >
+                <div style={{ marginTop: 8 }}>
+                  <PlusOutlined />
+                </div>
+              </Upload>
+            )}
           </Form.Item>
           <Form.Item
             label="内容"
