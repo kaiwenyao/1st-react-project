a6d2ccc60d1f9607d4e30c1278901942d9a74e29
Kaiwen Yao <kaiwen.yao.cn@outlook.com>
2025-11-28 13:37:16 +0000

    富文本编辑器 接入

--- react-jike/src/pages/Publish/index.js
+++ react-jike/src/pages/Publish/index.js
@@ -7,23 +7,26 @@ import {
   Input,
   Upload,
   Space,
-  Select
-} from 'antd'
-import { PlusOutlined } from '@ant-design/icons'
-import { Link } from 'react-router-dom'
-import './index.scss'
+  Select,
+} from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import "./index.scss";
+import ReactQuill from "react-quill-new";
+import "react-quill-new/dist/quill.snow.css";
 
-const { Option } = Select
+const { Option } = Select;
 
 const Publish = () => {
   return (
     <div className="publish">
       <Card
         title={
-          <Breadcrumb items={[
-            { title: <Link to={'/'}>首页</Link> },
-            { title: '发布文章' },
-          ]}
+          <Breadcrumb
+            items={[
+              { title: <Link to={"/"}>首页</Link> },
+              { title: "发布文章" },
+            ]}
           />
         }
       >
@@ -35,14 +38,14 @@ const Publish = () => {
           <Form.Item
             label="标题"
             name="title"
-            rules={[{ required: true, message: '请输入文章标题' }]}
+            rules={[{ required: true, message: "请输入文章标题" }]}
           >
             <Input placeholder="请输入文章标题" style={{ width: 400 }} />
           </Form.Item>
           <Form.Item
             label="频道"
             name="channel_id"
-            rules={[{ required: true, message: '请选择文章频道' }]}
+            rules={[{ required: true, message: "请选择文章频道" }]}
           >
             <Select placeholder="请选择文章频道" style={{ width: 400 }}>
               <Option value={0}>推荐</Option>
@@ -51,8 +54,14 @@ const Publish = () => {
           <Form.Item
             label="内容"
             name="content"
-            rules={[{ required: true, message: '请输入文章内容' }]}
-          ></Form.Item>
+            rules={[{ required: true, message: "请输入文章内容" }]}
+          >
+            <ReactQuill
+              className="publish-quill"
+              theme="snow"
+              placeholder="请输入文章内容"
+            />
+          </Form.Item>
 
           <Form.Item wrapperCol={{ offset: 4 }}>
             <Space>
@@ -64,7 +73,7 @@ const Publish = () => {
         </Form>
       </Card>
     </div>
-  )
-}
+  );
+};
 
-export default Publish
+export default Publish;
