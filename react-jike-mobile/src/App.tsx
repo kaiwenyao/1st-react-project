e91be766d059290d30180d06018e1a3aab9fab60
Kaiwen Yao <kaiwen.yao.cn@outlook.com>
2025-12-01 14:28:07 +0000

    完成网页 开始移动

---
+++ react-jike-mobile/src/App.tsx
@@ -0,0 +1,4 @@
+const App = () => {
+  return <div> this is app </div>
+}
+export default App
--- react-jike/craco.config.js
+++ react-jike/craco.config.js
@@ -1,9 +1,14 @@
 const path = require("path");
-
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const { whenProd, getPlugin, pluginByName } = require("@craco/craco");
 module.exports = {
   webpack: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
+    plugins: [
+      // 这里的逻辑是：只有当运行 npm run analyze 时，才启动分析插件
+      ...(process.env.ANALYZE === "true" ? [new BundleAnalyzerPlugin()] : []),
+    ],
   },
 };
